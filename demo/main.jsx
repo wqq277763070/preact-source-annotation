ebeeb659ff32fe8c8603fa96a77056677659dbea
yujingwyh <yujingwyh@163.com>
2020-04-10 11:03:15 +0800

    增加hooks源码注释

--- demo/main.jsx
+++ demo/main.jsx
@@ -7,35 +7,8 @@ import {
 	createRef
 } from '../src';
 
-class Root extends Component {
-	render() {
-		return <div>33333</div>;
-	}
-}
-
-class App extends Component {
-	$ref = createRef();
-
-	state = {
-		xx: 1
-	};
-
-	componentDidMount() {
-		setTimeout(() => {
-			this.setState({
-				xx: 0
-			});
-		}, 5000);
-	}
-
-	render() {
-		return (
-			<div>
-				23412341
-				{this.state.xx === 1 && <Root ref={this.$ref} />}
-			</div>
-		);
-	}
+function App() {
+	return <div>123</div>;
 }
 
 render(<App />, document.getElementById('app'));
